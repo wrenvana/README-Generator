09f2a927b73b9facbac8f080d1d672c692bee6c5
Wren Sanchez <demisa00@gmail.com>
2022-05-28 15:53:06 -0700

    Added array of questions.

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,50 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: "input",
+    name: "title",
+    message: "What is the title of your application?",
+},
+{
+    type: "input",
+    name: "purpose",
+    message: "What is the purpose of your application?",
+},
+{
+    type: "input",
+    name: "installation",
+    message: "How do you install your application?",
+},
+{
+    type: "input",
+    name: "usage",
+    message: "How do you use your application?",
+},
+{
+    type: "input",
+    name: "credits",
+    message: "Who are the collaborators?",
+},
+{
+    type: "input",
+    name: "github",
+    message: "What is your GitHub profile?",
+},
+{
+    type: "input",
+    name: "license",
+    message: "What license are you using?",
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile("README.md", data) {
+    if (err) console.log(err);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
