ae048d0239cd5355d573833631f366be9eca2c1e
Wren Sanchez <demisa00@gmail.com>
2022-05-28 16:50:55 -0700

    Started working on functions and installed inquirer.

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -26,6 +27,11 @@ const questions = [
 },
 {
     type: "input",
+    name: "media",
+    message: "Include screenshots or videos here.",
+},
+{
+    type: "input",
     name: "test",
     message: "How do you test your application?",
 },
@@ -48,16 +54,23 @@ const questions = [
     type: "input",
     name: "license",
     message: "What license are you using?",
+    choices: ["MIT", "GNU", "Apache 2.0"],
 },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile("README.md", data) {
-    if (err) console.log(err);
+function writeToFile(file, data) {
+    const markdownData = generateMarkdown;
+fs.appendFile("README.md", markdownData, err =>
+    err ? console.log(err) : console.log("Input received."));
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) =>
+    writeToFile("README.md", response)
+    );
+}
 
 // Function call to initialize app
 init();
